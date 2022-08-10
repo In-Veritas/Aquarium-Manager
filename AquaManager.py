6817a639daabf47bd7bbcf650ac1330ebea1b8e9
AxolotlOfTruth <gabriel.lara.verite@gmail.com>
2022-08-10 03:05:13 +0200

    Some fixes and another row added

--- AquaManager.py
+++ AquaManager.py
@@ -1,10 +1,11 @@
 import csv
+import sys
 from datetime import date
 import msvcrt as m
 def wait():
     m.getch()
 
-parameter_names = ["pH","Ammonia","NO2","NO3","Alkhalinity","Mg","Ca"]
+parameter_names = ["pH","Ammonia","NO2","NO3","PO4","Alkalinity","Mg","Ca"]
 parameter_dict={'Date': str(date.today().isoformat())}
 
 def is_number(s):
@@ -28,9 +29,7 @@ def insert_parameter():
         reader = csv.DictReader(csvfile)
         for row in reader:
             if row["Date"] == str(date.today().isoformat()):
-                print("This date has already been inserted")
-                wait()
-                return
+                sys.exit("This date has already been inserted.")
     for name in parameter_names:
         parameter = input("Please insert the " + name + "\n")
         while not is_number(parameter):
@@ -42,11 +41,11 @@ def insert_parameter():
 
 def insert_data():
     with open('AquaManager.csv', 'w',encoding='utf8', newline='') as csvfile:
-        fieldnames = ["Date","pH","NO2","NO3","Ammonia","Alkhalinity","Ca","Mg"]
+        fieldnames = ["Date","pH","NO2","NO3","PO4","Ammonia","Alkalinity","Ca","Mg"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
     with open('AquaManager.csv', 'a',encoding='utf8', newline='') as csvfile:
-        fieldnames = ["Date","pH","NO2","NO3","Ammonia","Alkhalinity","Ca","Mg"]
+        fieldnames = ["Date","pH","NO2","NO3","PO4","Ammonia","Alkalinity","Ca","Mg"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(parameter_dict)
 
